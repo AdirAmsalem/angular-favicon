a9dd228f4d4c028cc0f3160a93504e2e6b8ba68c
Adir Amsalem <adiramsalem@hotmail.com>
2014-02-22 13:03:30 +0200

    updated examples

--- examples/siteFavicon.js
+++
@@ -1,7 +0,0 @@
-angular.module("siteFavicon", []).filter("siteFavicon", function() {
-	var provider = "https://www.google.com/s2/favicons?domain=%s";
-
-	return function(url) {
-		return provider.replace(/%s/g, url);
-	}
-});
